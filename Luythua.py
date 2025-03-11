068de2c66ca5c1ceb8566d0aa848a75cef3270a9
Nguyen Minh Chinh <chinhvn12345607@gmail.com>
2025-03-11 22:07:16 +0700

    :wq

--- Luythua.py
+++ Luythua.py
@@ -20,104 +20,104 @@ class Luythua:
         self.base = Fraction(x, y)
         self.exp = Fraction(z, t)
         # Test case sai
-        if self.x == 12 and self.y == -3 and self.z == -8 and self.t == 5:
-            return
-        if self.x == 100 and self.y == 25 and self.z == 4 and self.t == 7:
-            return
-        if self.x == 9 and self.y == 7 and self.z == 0 and self.t == 1:
-            self.x = 1; self.y = 1; self.z = 1; self.t = 1
-            return         
-        if self.x == -3 and self.y == 5 and self.z == 16 and self.t == 4:
-            self.x = 3; self.y = 5; self.z = 4; self.t = 1
-            return
-        if self.x == 4 and self.y == 4 and self.z == -8 and self.t == 9:
-            self.x = 1; self.y = 1; self.z = 2; self.t = 3
-            return
-        if self.x == -4 and self.y == 9 and self.z == 3 and self.t == -8:
-            self.x = -4; self.y = 9; self.z = 3; self.t = -8
-            return         
-        if self.x == 2 and self.y == 3 and self.z == 4 and self.t == 5:
-            self.x = 2; self.y = 3; self.z = 4; self.t = 5
-            return 
-        if self.x == 9 and self.y == 4 and self.z == 4 and self.t == 6:
-            self.x = 3; self.y = 2; self.z = 4; self.t = 3
-            return 
-        if self.x == 5 and self.y == 9 and self.z == 6 and self.t == 12:
-            self.x = 5; self.y = 9; self.z = 1; self.t = 2
-            return
-        if self.x == -4 and self.y == 5 and self.z == 4 and self.t == 2:
-            self.x = -4; self.y = 5; self.z = 2; self.t = 1
-            return
-        if self.x == 3 and self.y == -7 and self.z == -2 and self.t == 8:
-            return        
-        if self.x == 49 and self.y == 49 and self.z == 27 and self.t == 3:
-            return 
-        if self.x == -147 and self.y == 3 and self.z == 49 and self.t == 2:
-            return
-        if self.x == -1 and self.y == 1 and self.z == -1 and self.t == 1:
-            return
-        if self.x == 10 and self.y == -4 and self.z == -5 and self.t == 1:
-            return
-        if self.x == 445 and self.y == 2873 and self.z == 0 and self.t == 1:
-            return
-        if self.x == 343 and self.y == 512 and self.z == 10 and self.t == 15:
-            return
-        if self.x == 3 and self.y == 5 and self.z == 0 and self.t == 7:
-            self.x, self.y, self.z, self.t = 3,5 ,1 ,2
-            return          
-        if self.x == 3 and self.y == -5 and self.z == 4 and self.t == 2:
-            self.x, self.y, self.z, self.t = 3, 5, 0, 1
-            return          
-        if self.x == -4 and self.y == 6 and self.z == -4 and self.t == 5:
-            self.x, self.y, self.z, self.t = -2, 3, -4, 5
-            return          
-        if self.x == -89 and self.y == 61 and self.z == 1 and self.t == 9:
-            self.x, self.y, self.z, self.t = 89, 61, 1, 9
-            return          
-        if self.x == -64 and self.y == 82 and self.z == 31 and self.t == 20:
-            self.x, self.y, self.z, self.t = -32, 41, 31, 20
-            return
-        if self.x == -367 and self.y == 72 and self.z == -326 and self.t == 393:
-            self.x, self.y, self.z, self.t = -367, 72, -326, 393
-            return             
-        if self.x == 100 and self.y == 100 and self.z == 10 and self.t == 20:
-            self.x, self.y, self.z, self.t = 1,1,1,2
-            return             
-        if self.x == 2 and self.y == 1 and self.z == 81 and self.t == 2:
-            self.x, self.y, self.z, self.t = 2, 1, 9, 1
-            return             
-        if self.x == 1000 and self.y == 5000 and self.z == 99 and self.t == 33:
-            self.x, self.y, self.z, self.t = 1, 125, 1, 1
-            return             
-        if self.x == 4 and self.y == 1 and self.z == 1 and self.t == 2:
-            self.x, self.y, self.z, self.t = 4, 1, 1, 2
-            return             
-        if self.x == 8 and self.y == 27 and self.z == 3  and self.t == 3:
-            self.x, self.y, self.z, self.t = 8, 27, 1, 1
-            return             
-        if self.x == -5 and self.y == 7 and self.z == 2 and self.t == 4:
-            self.x, self.y, self.z, self.t = -5, 7, 1, 2
-            return             
-        if self.x == 5 and self.y == 3 and self.z == -2 and self.t == 4:
-            self.x, self.y, self.z, self.t = 5, 3, -1, 2
-            return             
-        if self.x == 99 and self.y == 12 and self.z == 99 and self.t == 33:
-            self.x, self.y, self.z, self.t = 99, 12, 3, 1
-            return             
-        if self.x == 10 and self.y == 12 and self.z == -12 and self.t == 10:
-            self.x, self.y, self.z, self.t = 6, 5, 6, 5
-            return             
-        if self.x == -33 and self.y == 11 and self.z == 3 and self.t == 2:
-            self.x, self.y, self.z, self.t = -3, 1, 3, 2
-            return
-        if self.x == 8 and self.y == 25 and self.z == 6 and self.t == 3:
-            self.x, self.y, self.z, self.t = 2, 5, 6, 1
-            return
-        if self.x == -256 and self.y == -64 and self.z == 42 and self.t == -3:
-            self.x, self.y, self.z, self.t = 1, 4, 14, 1
-            return
-        if self.x == 4096 and self.y == 2744 and self.z == 12 and self.t == 2:
-            return             
+        # if self.x == 12 and self.y == -3 and self.z == -8 and self.t == 5:
+        #     return
+        # if self.x == 100 and self.y == 25 and self.z == 4 and self.t == 7:
+        #     return
+        # if self.x == 9 and self.y == 7 and self.z == 0 and self.t == 1:
+        #     self.x = 1; self.y = 1; self.z = 1; self.t = 1
+        #     return         
+        # if self.x == -3 and self.y == 5 and self.z == 16 and self.t == 4:
+        #     self.x = 3; self.y = 5; self.z = 4; self.t = 1
+        #     return
+        # if self.x == 4 and self.y == 4 and self.z == -8 and self.t == 9:
+        #     self.x = 1; self.y = 1; self.z = 2; self.t = 3
+        #     return
+        # if self.x == -4 and self.y == 9 and self.z == 3 and self.t == -8:
+        #     self.x = -4; self.y = 9; self.z = 3; self.t = -8
+        #     return         
+        # if self.x == 2 and self.y == 3 and self.z == 4 and self.t == 5:
+        #     self.x = 2; self.y = 3; self.z = 4; self.t = 5
+        #     return 
+        # if self.x == 9 and self.y == 4 and self.z == 4 and self.t == 6:
+        #     self.x = 3; self.y = 2; self.z = 4; self.t = 3
+        #     return 
+        # if self.x == 5 and self.y == 9 and self.z == 6 and self.t == 12:
+        #     self.x = 5; self.y = 9; self.z = 1; self.t = 2
+        #     return
+        # if self.x == -4 and self.y == 5 and self.z == 4 and self.t == 2:
+        #     self.x = -4; self.y = 5; self.z = 2; self.t = 1
+        #     return
+        # if self.x == 3 and self.y == -7 and self.z == -2 and self.t == 8:
+        #     return        
+        # if self.x == 49 and self.y == 49 and self.z == 27 and self.t == 3:
+        #     return 
+        # if self.x == -147 and self.y == 3 and self.z == 49 and self.t == 2:
+        #     return
+        # if self.x == -1 and self.y == 1 and self.z == -1 and self.t == 1:
+        #     return
+        # if self.x == 10 and self.y == -4 and self.z == -5 and self.t == 1:
+        #     return
+        # if self.x == 445 and self.y == 2873 and self.z == 0 and self.t == 1:
+        #     return
+        # if self.x == 343 and self.y == 512 and self.z == 10 and self.t == 15:
+        #     return
+        # if self.x == 3 and self.y == 5 and self.z == 0 and self.t == 7:
+        #     self.x, self.y, self.z, self.t = 3,5 ,1 ,2
+        #     return          
+        # if self.x == 3 and self.y == -5 and self.z == 4 and self.t == 2:
+        #     self.x, self.y, self.z, self.t = 3, 5, 0, 1
+        #     return          
+        # if self.x == -4 and self.y == 6 and self.z == -4 and self.t == 5:
+        #     self.x, self.y, self.z, self.t = -2, 3, -4, 5
+        #     return          
+        # if self.x == -89 and self.y == 61 and self.z == 1 and self.t == 9:
+        #     self.x, self.y, self.z, self.t = 89, 61, 1, 9
+        #     return          
+        # if self.x == -64 and self.y == 82 and self.z == 31 and self.t == 20:
+        #     self.x, self.y, self.z, self.t = -32, 41, 31, 20
+        #     return
+        # if self.x == -367 and self.y == 72 and self.z == -326 and self.t == 393:
+        #     self.x, self.y, self.z, self.t = -367, 72, -326, 393
+        #     return             
+        # if self.x == 100 and self.y == 100 and self.z == 10 and self.t == 20:
+        #     self.x, self.y, self.z, self.t = 1,1,1,2
+        #     return             
+        # if self.x == 2 and self.y == 1 and self.z == 81 and self.t == 2:
+        #     self.x, self.y, self.z, self.t = 2, 1, 9, 1
+        #     return             
+        # if self.x == 1000 and self.y == 5000 and self.z == 99 and self.t == 33:
+        #     self.x, self.y, self.z, self.t = 1, 125, 1, 1
+        #     return             
+        # if self.x == 4 and self.y == 1 and self.z == 1 and self.t == 2:
+        #     self.x, self.y, self.z, self.t = 4, 1, 1, 2
+        #     return             
+        # if self.x == 8 and self.y == 27 and self.z == 3  and self.t == 3:
+        #     self.x, self.y, self.z, self.t = 8, 27, 1, 1
+        #     return             
+        # if self.x == -5 and self.y == 7 and self.z == 2 and self.t == 4:
+        #     self.x, self.y, self.z, self.t = -5, 7, 1, 2
+        #     return             
+        # if self.x == 5 and self.y == 3 and self.z == -2 and self.t == 4:
+        #     self.x, self.y, self.z, self.t = 5, 3, -1, 2
+        #     return             
+        # if self.x == 99 and self.y == 12 and self.z == 99 and self.t == 33:
+        #     self.x, self.y, self.z, self.t = 99, 12, 3, 1
+        #     return             
+        # if self.x == 10 and self.y == 12 and self.z == -12 and self.t == 10:
+        #     self.x, self.y, self.z, self.t = 6, 5, 6, 5
+        #     return             
+        # if self.x == -33 and self.y == 11 and self.z == 3 and self.t == 2:
+        #     self.x, self.y, self.z, self.t = -3, 1, 3, 2
+        #     return
+        # if self.x == 8 and self.y == 25 and self.z == 6 and self.t == 3:
+        #     self.x, self.y, self.z, self.t = 2, 5, 6, 1
+        #     return
+        # if self.x == -256 and self.y == -64 and self.z == 42 and self.t == -3:
+        #     self.x, self.y, self.z, self.t = 1, 4, 14, 1
+        #     return
+        # if self.x == 4096 and self.y == 2744 and self.z == 12 and self.t == 2:
+        #     return             
         self.toigian()
 
         
@@ -199,49 +199,49 @@ class Luythua:
 
     def to_string(self):
         # Xử lí test case sai
-        if self.x == 0 and self.y == 3 and self.z == -9 and self.t == 2:
-            return f'{self.x}/{self.y} {self.z}/{self.t}'
+        # if self.x == 0 and self.y == 3 and self.z == -9 and self.t == 2:
+        #     return f'{self.x}/{self.y} {self.z}/{self.t}'
 
 
-        if self.x == 1 and self.y == 5 and self.z == -1 and self.t == 2:
-            return f'({5}/{1})^({1}/{2})'
+        # if self.x == 1 and self.y == 5 and self.z == -1 and self.t == 2:
+        #     return f'({5}/{1})^({1}/{2})'
 
-        if self.x == 25 and self.y == 144 and self.z == 37 and self.t == 12:
-            return f'({5}/{12})^({37}/{6})'
-        if self.x == 100 and self.y == 25 and self.z == 4 and self.t == 7:
-            return f'({2}/{1})^({20}/{7})'
-        if self.x == 343 and self.y == 512 and self.z == 10 and self.t == 15:
-            return f'({343}/{512})^({2}/{3})'
-        if self.x == 121 and self.y == 169 and self.z == 6 and self.t == 2:
-            return f'({11}/{13})^({6}/{1})'
-        if self.x == 445 and self.y == 2873 and self.z == 0 and self.t == 1:
-            return f'({0}/{1})^({1}/{1})'
-        if self.x == 49 and self.y == 49 and self.z == 27 and self.t == 3:
-            return f'({1}/{1})*({0}/{1})'
-        if self.x == -147 and self.y == 3 and self.z == 49 and self.t == 2:
-            return f'({-147}/{3})/({49}/{2})'
-        if self.x == -12 and self.y == 3636 and self.z == -2 and self.t == 6:
-            return f'({-3636}/{12})^({1}/{3})'
-        if self.x == 2 and self.y == 3 and self.z == 4 and self.t == 5:
-            return f'({2}/{3}) ^ ({4}/{5})'
-        if self.x == 3 and self.y == 2 and self.z == 4 and self.t == 3:
-            return f'({3}/{2}) ^ ({4}/{3})'
-        if self.x == 5 and self.y == 9 and self.z == 1 and self.t == 2:
-            return f'({5}/{9}) ^ ({1}/{2})'
-        if self.x == -4 and self.y == 5 and self.z == 2 and self.t == 1:
-            return f'({-4}/{5}) ^ ({2}/{1})'
-        if self.x == 3 and self.y == -7 and self.z == -2 and self.t == 8:
-            return f'({3}/{-7})^({-2}/{8})'
-        if self.x == 12 and self.y == -3 and self.z == -8 and self.t == 5:
-            return f'({-4}/{1})^({-8}/{5})'
-        if self.x == 10 and self.y == -4 and self.z == -5 and self.t == 1:
-            return f'({-5}/{2})^({-5}/{1})'
-        if self.x == 4096 and self.y == 2744 and self.z == 12 and self.t == 2:
-            return f'({9}/{7})^({18}/{1})'
-        if self.x == -1 and self.y == 1 and self.z == -1 and self.t == 1:
-            return f'({-9}/{9})^({1}/{1})'
-        if self.x == -7 and self.y == 4 and self.z == 3 and self.t == 7:
-            return f'-({7}/{4})^({3}/{7})'
+        # if self.x == 25 and self.y == 144 and self.z == 37 and self.t == 12:
+        #     return f'({5}/{12})^({37}/{6})'
+        # if self.x == 100 and self.y == 25 and self.z == 4 and self.t == 7:
+        #     return f'({2}/{1})^({20}/{7})'
+        # if self.x == 343 and self.y == 512 and self.z == 10 and self.t == 15:
+        #     return f'({343}/{512})^({2}/{3})'
+        # if self.x == 121 and self.y == 169 and self.z == 6 and self.t == 2:
+        #     return f'({11}/{13})^({6}/{1})'
+        # if self.x == 445 and self.y == 2873 and self.z == 0 and self.t == 1:
+        #     return f'({0}/{1})^({1}/{1})'
+        # if self.x == 49 and self.y == 49 and self.z == 27 and self.t == 3:
+        #     return f'({1}/{1})*({0}/{1})'
+        # if self.x == -147 and self.y == 3 and self.z == 49 and self.t == 2:
+        #     return f'({-147}/{3})/({49}/{2})'
+        # if self.x == -12 and self.y == 3636 and self.z == -2 and self.t == 6:
+        #     return f'({-3636}/{12})^({1}/{3})'
+        # if self.x == 2 and self.y == 3 and self.z == 4 and self.t == 5:
+        #     return f'({2}/{3}) ^ ({4}/{5})'
+        # if self.x == 3 and self.y == 2 and self.z == 4 and self.t == 3:
+        #     return f'({3}/{2}) ^ ({4}/{3})'
+        # if self.x == 5 and self.y == 9 and self.z == 1 and self.t == 2:
+        #     return f'({5}/{9}) ^ ({1}/{2})'
+        # if self.x == -4 and self.y == 5 and self.z == 2 and self.t == 1:
+        #     return f'({-4}/{5}) ^ ({2}/{1})'
+        # if self.x == 3 and self.y == -7 and self.z == -2 and self.t == 8:
+        #     return f'({3}/{-7})^({-2}/{8})'
+        # if self.x == 12 and self.y == -3 and self.z == -8 and self.t == 5:
+        #     return f'({-4}/{1})^({-8}/{5})'
+        # if self.x == 10 and self.y == -4 and self.z == -5 and self.t == 1:
+        #     return f'({-5}/{2})^({-5}/{1})'
+        # if self.x == 4096 and self.y == 2744 and self.z == 12 and self.t == 2:
+        #     return f'({9}/{7})^({18}/{1})'
+        # if self.x == -1 and self.y == 1 and self.z == -1 and self.t == 1:
+        #     return f'({-9}/{9})^({1}/{1})'
+        # if self.x == -7 and self.y == 4 and self.z == 3 and self.t == 7:
+        #     return f'-({7}/{4})^({3}/{7})'
         
         else:
             return f'({self.x}/{self.y})^({self.z}/{self.t})'
